df5f5c0f055e79e9c3f0d15a339b44d83a493ef5
Ibrahim Kelly <ibrahimkelly99outlook.com>
2026-06-04 15:41:05 +0000

    Update SHA256 for thl-kalix v1.0.0 dmg

--- Casks/thl-kalix.rb
+++ Casks/thl-kalix.rb
@@ -3,7 +3,7 @@ cask "thl-kalix" do
   
   # Replace with the actual SHA256 checksum of your compiled macOS DMG file.
   # Generate it via: shasum -a 256 THL-KALIX-1.0.0.dmg
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "89bd6040318ba7eae8d9908797bd2258e6453c0d59f3761ae8d266d216c1772b"
 
   # Replace this with your actual hosted GitHub releases DMG file download link.
   url "https://github.com/thltechnologies/Kaliss-By-THL/releases/download/v#{version}/THL-KALIX-#{version}.dmg"
