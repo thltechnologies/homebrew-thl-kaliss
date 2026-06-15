1b75e2b79f218a60e36ac26b0c7f1c87f655d415
I-K(***)Y <94073143+ibrahimkelly@users.noreply.github.com>
2026-06-15 16:32:43 +0000

    Update

--- Casks/thl-kaliss.rb
+++ Casks/thl-kaliss.rb
@@ -1,21 +1,13 @@
 cask "thl-kaliss" do
-  version "1.0.0"
+  version "1.0.3"
+  sha256 :no_check # On désactive la vérification du hash pour l'instant
+
+  # Assurez-vous que l'URL correspond au lien de votre Release GitHub
+  url "https://github.com/thltechnologies/THL-KALISS-Desktop/releases/download/v#{version}/THL-KALISS-Desktop-#{version}-mac.zip"
   
-  # Replace with the actual SHA256 checksum of your compiled macOS DMG file.
-  # Generate it via: shasum -a 256 THL-KALIX-1.0.0.dmg
-  sha256 "89bd6040318ba7eae8d9908797bd2258e6453c0d59f3761ae8d266d216c1772b"
+  name "THL Kaliss"
+  desc "Plateforme de Core Banking Simple & Efficace"
+  homepage "https://thl-kaliss.web.app/"
 
-  # Replace this with your actual hosted GitHub releases DMG file download link.
-  url "https://github.com/thltechnologies/Kaliss-By-THL/releases/download/v#{version}/THL-KALIX-#{version}.dmg"
-  name "THL-KALISS"
-  desc "Système Core Banking Desktop autonome et distribué par THL Technologies"
-  homepage "https://github.com/thltechnologies/Kaliss-By-THL"
-
-  app "THL-KALIX.app"
-
-  zap trash: [
-    "~/Library/Application Support/thl-kalix",
-    "~/Library/Preferences/com.thltechnologies.kalix.plist",
-    "~/Library/Saved Application State/com.thltechnologies.kalix.savedState",
-  ]
+  app "THL KALISS.app"
 end
